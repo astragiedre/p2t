db205e14dc3bfe9a84569e540b307b819ff1d6f4
domantasjurkus <domantas.jurkus@gmail.com>
2017-02-20 12:51:42 +0000

    Added temp converter

---
+++ doms/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Convert temperature to Kelvin
+int toKelvin(int celcius) {
+	// TODO
+}
+
+int main() {
+
+	printf("%d in Celcius is %d in Kelvin\n", 10, toKelvin(10));
+	printf("%d in Celcius is %d in Kelvin\n", 23, toKelvin(23));
+	printf("%d in Celcius is %d in Kelvin\n", 50, toKelvin(50));
+
+	return 0;
+}
